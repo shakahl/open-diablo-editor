1664fcf5f5e82658ce728f6d51e6674974f4654f
Matthew R. Karlsen <matthew@karlsen.me.uk>
2015-06-12 21:14:52 +0100

    Made convertFourBytesToNumber() in BinEditHelper more succinct.

--- OpenDiabloEditor/src/uk/me/karlsen/ode/BinEditHelper.java
+++ OpenDiabloEditor/src/uk/me/karlsen/ode/BinEditHelper.java
@@ -99,30 +99,29 @@ public class BinEditHelper {
 		return value;
 	}
 	
-	//FIXME -- move to BinEditHelper
+	/**
+	 * Converts four bytes on the specified array to a long.
+	 * The particular bytes in question are determined by the offset
+	 * (from the start of the array).
+	 * 
+	 * @param holdingArray : the array to get the four bytes from
+	 * @param offset : the offset of the bytes on the array
+	 * @return a long value converted from the four bytes
+	 */
 	public long convertFourBytesToNumber(byte[] holdingArray, int offset){
-		String byte0 = Integer.toHexString(holdingArray[offset+3] & 0xFF);
-		String byte1 = Integer.toHexString(holdingArray[offset+2] & 0xFF);
-		String byte2 = Integer.toHexString(holdingArray[offset+1] & 0xFF);
-		String byte3 = Integer.toHexString(holdingArray[offset] & 0xFF);
-		if(byte0.length() < 2){
-			byte0 = "0" + byte0;
+		StringBuilder sb = new StringBuilder();
+		for(int i = 3; i > -1; i--){
+			String byteValue = Integer.toHexString(holdingArray[offset+i] & 0xFF);
+			if(byteValue.length() == 1){
+				sb.append(0);
+			}
+			sb.append(byteValue);
 		}
-		if(byte1.length() < 2){
-			byte1 = "0" + byte1;
-		}
-		if(byte2.length() < 2){
-			byte2 = "0" + byte2;
-		}
-		if(byte3.length() < 2){
-			byte3 = "0" + byte3;
-		}
-		String str = byte0 + byte1 + byte2 + byte3;
+		String str = sb.toString();
 		long value = Long.parseLong(str, 16);
 		return value;
 	}
 	
-	//FIXME -- move to BinEditHelper
 	public int convertTwoBytesToInt(int... bytes){
 		int value = -1;
 		String byte0 = Integer.toHexString(bytes[1] & 0xFF);
